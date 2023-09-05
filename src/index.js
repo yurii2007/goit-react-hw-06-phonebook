09dea3a931cf0a7a52b8b0a839ae5a7d09faa7a8
yurii2007 <yurash862@gmail.com>
2023-09-05 22:46:31 +0300

    add prettier formatting

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
 import { persistore, store } from 'redux/store';
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <PersistGate persistor={persistore}>
-      <App />
+      <PersistGate persistor={persistore}>
+        <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>
